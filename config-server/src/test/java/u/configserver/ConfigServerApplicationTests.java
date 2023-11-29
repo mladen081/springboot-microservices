9272a0795c29a5a1ef98ff54a74aa2a9e75b4605
mladen081 <jovicmladen094@gmail.com>
2023-11-29 23:34:42 +0100

    016 Centralized Configurations using Spring Cloud Config Server.

---
+++ config-server/src/test/java/u/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package u.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
